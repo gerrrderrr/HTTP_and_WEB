3db229777a13a218dd20aac4a271aadb46351576
Olga Kozlova <o.o.olga.kozlova@gmail.com>
2022-11-26 16:45:14 +0300

    Added new branch

---
+++ src/main/java/Request.java
@@ -0,0 +1,17 @@
+public class Request {
+    private final String method;
+    private final String path;
+
+    public Request(String requestMethod, String requestPath) {
+        this.method = requestMethod;
+        this.path = requestPath;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+}
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -4,14 +4,20 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
 public class Server {
     private static final String PATH_TO_SETTINGS = "src/main/resources/settings.txt";
     private static final int THREAD_POOL_SIZE = 64;
+    private final Map<String, Map<String, MyHandler>> handlers;
+    private BufferedReader in;
+    private BufferedOutputStream out;
 
     public Server() {
+        handlers = new HashMap<>();
     }
 
     public void serverStart() {
@@ -29,8 +35,8 @@ public class Server {
     }
 
     private void taskStart(Socket socket) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new BufferedOutputStream(socket.getOutputStream());
         ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(THREAD_POOL_SIZE);
         executor.execute(new Thread(() -> {
             try {
@@ -40,52 +46,18 @@ public class Server {
                 if (parts.length != 3) {
                     Thread.currentThread().interrupt();
                 }
-
-                final String path = parts[1];
-                if (!Main.validPaths.contains(path)) {
-                    out.write((
-                            """
-                                    HTTP/1.1 404 Not Found\r
-                                    Content-Length: 0\r
-                                    Connection: close\r
-                                    \r
-                                    """
-                    ).getBytes());
-                    out.flush();
+                Request request = new Request(parts[0], parts[1]);
+                if (handlers.containsKey(request.getMethod())) {
+                    if (handlers.get(request.getMethod()).containsKey(request.getPath())) {
+                        handlers.get(request.getMethod()).get(request.getPath()).handle(request, out);
+                    } else {
+                        notAvailableHandler("404", "Not Found");
+                    }
+                } else {
+                    notAvailableHandler("404", "Not Found");
                 }
-
-                final Path filePath = Path.of("src/main/resources", path);
-                final String mimeType = Files.probeContentType(filePath);
-
-                if (path.equals("/classic.html")) {
-                    final String template = Files.readString(filePath);
-                    final byte[] content = template.replace(
-                            "{time}",
-                            LocalDateTime.now().toString()
-                    ).getBytes();
-                    out.write((
-                            "HTTP/1.1 200 OK\r\n" +
-                                    "Content-Type: " + mimeType + "\r\n" +
-                                    "Content-Length: " + content.length + "\r\n" +
-                                    "Connection: close\r\n" +
-                                    "\r\n"
-                    ).getBytes());
-                    out.write(content);
-                    out.flush();
-                }
-
-                final long length = Files.size(filePath);
-                out.write((
-                        "HTTP/1.1 200 OK\r\n" +
-                                "Content-Type: " + mimeType + "\r\n" +
-                                "Content-Length: " + length + "\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n"
-                ).getBytes());
-                Files.copy(filePath, out);
-                out.flush();
             } catch (IOException e) {
-                System.out.println(e.getMessage());
+                throw new RuntimeException(e);
             }
         }));
     }
@@ -104,5 +76,70 @@ public class Server {
         } else {
             return port;
         }
+    }
+
+    public void notAvailableHandler(String errorCode, String statusMessage) throws IOException {
+        out.write((
+                "HTTP/1.1 " + errorCode + " " +
+                        statusMessage + "\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        out.flush();
+    }
+
+
+    public void basicHandler(String path) throws IOException {
+        Path filePath = returnPath(path);
+        String mimeType = returnMimeType(filePath);
+        if (path.equals("/classic.html")) {
+
+            final String template = Files.readString(filePath);
+            final byte[] content = template.replace(
+                    "{time}",
+                    LocalDateTime.now().toString()
+            ).getBytes();
+            out.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + content.length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            out.write(content);
+            out.flush();
+        }
+        final long length = Files.size(filePath);
+        out.write((
+                "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: " + mimeType + "\r\n" +
+                        "Content-Length: " + length + "\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        Files.copy(filePath, out);
+        out.flush();
+    }
+
+    public void addHandler(String method, String path, MyHandler handler) {
+        if (!handlers.containsKey(method)) {
+            handlers.put(method, new HashMap<>());
+            handlers.get(method).put(path, handler);
+        } else {
+            if (!handlers.get(method).containsKey(path)) {
+                handlers.get(method).put(path, handler);
+            } else {
+                System.out.println("Handler exist");
+            }
+        }
+    }
+
+    private Path returnPath(String path) {
+        return Path.of("src/main/resources", path);
+    }
+
+    private String returnMimeType(Path filePath) throws IOException {
+        return Files.probeContentType(filePath);
     }
 }
